6533aeb9aa37e4e7a6411d3a63fa58ea843bed7b
Stijn Peeters <schwarzie2478@hotmail.com>
2019-02-24 19:33:21 +0100

    Added Health Monitoring

--- Example01BackEnd/Startup.cs
+++ Example01BackEnd/Startup.cs
@@ -1,17 +1,25 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Example01.Data;
+
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace Example01BackEnd
@@ -39,13 +47,12 @@ namespace Example01BackEnd
                 c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
             });
 
-            var connection = @"Server=(localdb)\mssqllocaldb;Database=Example01;Trusted_Connection=True;ConnectRetryCount=0";
+            var connectionString = @"Server=(localdb)\mssqllocaldb;Database=Example01;Trusted_Connection=True;ConnectRetryCount=0";
             services.AddDbContext<ExampleDbContext>
-                (options => options.UseSqlServer(connection));
+                (options => options.UseSqlServer(connectionString));
 
-    services.AddHealthChecks()
-        .AddSqlServer(connection);
-               services.AddHealthChecksUI();
+            services.AddHealthChecks()
+                .AddDbContextCheck<ExampleDbContext>("localDb");
 
         }
 
@@ -61,6 +68,11 @@ namespace Example01BackEnd
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseHealthChecks("/health", new HealthCheckOptions()
+            {
+                // WriteResponse is a delegate used to write the response.
+                ResponseWriter = WriteResponse
+            });
 
             app.UseHttpsRedirection();
 
@@ -76,12 +88,29 @@ namespace Example01BackEnd
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
             
-                    app.UseHealthChecksUI();
+
             app.UseCors(builder =>
     builder.WithOrigins("http://localhost:65320")
            .AllowAnyHeader()
            .AllowAnyMethod()
     );
+        }
+
+        private static Task WriteResponse(HttpContext httpContext,
+    HealthReport result)
+        {
+            httpContext.Response.ContentType = "application/json";
+
+            var json = new JObject(
+                new JProperty("status", result.Status.ToString()),
+                new JProperty("results", new JObject(result.Entries.Select(pair =>
+                    new JProperty(pair.Key, new JObject(
+                        new JProperty("status", pair.Value.Status.ToString()),
+                        new JProperty("description", pair.Value.Description),
+                        new JProperty("data", new JObject(pair.Value.Data.Select(
+                            p => new JProperty(p.Key, p.Value))))))))));
+            return httpContext.Response.WriteAsync(
+                json.ToString(Formatting.Indented));
         }
     }
 }
